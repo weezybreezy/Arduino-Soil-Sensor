67edb4913472faba4fbe24050ad09badc8b4311d
alutkenh <alutkenh@yahoo.com>
2017-01-07 19:10:12 -0500

    Add files via upload

---
+++ soil_moisture_v1.ino
@@ -0,0 +1,13 @@
+//Alexander Lutkenhouse and Mike Weiss's Soil Moisture Sensor
+//January 7, 2017
+
+// These constants won't change:
+const int sensorPin = A0;    // pin that the sensor is attached to
+const int ledPin = 9;        // pin that the LED is attached to
+
+// variables:
+int sensorValue = 0;         // the sensor value
+int sensorMin = 1023;        // minimum sensor value
+int sensorMax = 0;           // maximum sensor value
+
+void setup(){
